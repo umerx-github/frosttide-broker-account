a31b6d1dc70f30c026ae549d38cf61d337df5b29
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-05-11 12:04:19 +0000

    Add tests

--- src/classes/EventProcessor.spec.ts
+++ src/classes/EventProcessor.spec.ts
@@ -3,22 +3,31 @@ import { expect, use } from 'chai';
 import sinonChai from 'sinon-chai';
 import EventProcessor from './EventProcessor.js';
 import { db } from '../connections/database.js';
+import { seed } from '../seeds/test.js';
 
 use(sinonChai);
 
+function getDependencies() {
+    const producer = {
+        sendMessage: sinon.spy(),
+    };
+    const consumer = {
+        addOnMessageHandler: sinon.spy(),
+    };
+    const eventProcessor = new EventProcessor({
+        db,
+        producer,
+        consumer,
+    });
+    return { producer, consumer, eventProcessor }
+}
+
 describe('EventProcessor', () => {
-    it('processes events with a connection 1x', async () => {
-        const producer = {
-            sendMessage: sinon.spy(),
-        };
-        const consumer = {
-            addOnMessageHandler: sinon.spy(),
-        };
-        const eventProcessor = new EventProcessor({
-            db,
-            producer,
-            consumer,
-        });
+    beforeEach(async () => {
+        await seed();
+    })
+    it('handles RequestedAccountList 1x', async () => {
+        const { producer, eventProcessor } = getDependencies();
         await eventProcessor.processEvent({
             value: Buffer.from(
                 JSON.stringify({
@@ -39,7 +48,7 @@ describe('EventProcessor', () => {
                             lock: {
                                 versionId: 0,
                                 proofOfInclusionBTreeSerialized:
-                                    '{"t":3,"root":{"isLeaf":true,"keys":[2],"children":[]}}',
+                                    '{"t":3,"root":{"isLeaf":false,"keys":[4],"children":[{"isLeaf":true,"keys":[2,3],"children":[]},{"isLeaf":true,"keys":[5,6,7],"children":[]}]}}',
                             },
                             object: {
                                 id: 1,
@@ -52,7 +61,7 @@ describe('EventProcessor', () => {
                             lock: {
                                 versionId: 0,
                                 proofOfInclusionBTreeSerialized:
-                                    '{"t":3,"root":{"isLeaf":true,"keys":[2],"children":[]}}',
+                                    '{"t":3,"root":{"isLeaf":true,"keys":[4],"children":[]}}',
                             },
                             object: {
                                 id: 2,
@@ -66,18 +75,78 @@ describe('EventProcessor', () => {
             }),
         });
     });
-    it('processes events with a connection 2x', async () => {
-        const producer = {
-            sendMessage: sinon.spy(),
-        };
-        const consumer = {
-            addOnMessageHandler: sinon.spy(),
-        };
-        const eventProcessor = new EventProcessor({
-            db,
-            producer,
-            consumer,
+    it('handles RequestedAccountCreateIntent', async () => {
+        const { producer, eventProcessor } = getDependencies();
+        await eventProcessor.processEvent({
+            value: Buffer.from(JSON.stringify({
+                "eventType": "RequestedAccountCreateIntent"
+            }))
+        })
+        expect(producer.sendMessage).to.have.been.calledWith({
+            key: 'myKey',
+            value: JSON.stringify({
+                eventType: 'AcknowledgedAccountCreateIntent',
+                data: {
+                    request: { eventType: 'RequestedAccountCreateIntent' },
+                    lock: {
+                        versionId: 1,
+                        proofOfInclusionBTreeSerialized: '{"t":3,"root":{"isLeaf":true,"keys":[1],"children":[]}}'
+                    }
+                }
+            }),
+        })
+    })
+    it('handles RequestedAccountCreate', async () => {
+        const { producer, eventProcessor } = getDependencies();
+        await eventProcessor.processEvent({
+            value: Buffer.from(JSON.stringify({
+                eventType: "RequestedAccountCreate",
+                messageId: 3,
+                lastReadVersionId: 1,
+                data: {
+                    platform: "Alpaca",
+                    platformAccountId: "abc123",
+                    platformAPIKey: "xyz321"
+                }
+            }))
         });
+        expect(producer.sendMessage).to.have.been.calledWith({
+            key: 'myKey',
+            value: JSON.stringify({
+                "eventType": "AcknowledgedAccountCreate",
+                "data": {
+                    "request": {
+                        "eventType": "RequestedAccountCreate",
+                        "messageId": 3,
+                        "lastReadVersionId": 1,
+                        "data": {
+                            "platform": "Alpaca",
+                            "platformAccountId": "abc123",
+                            "platformAPIKey": "xyz321"
+                        }
+                    },
+                    "lock": {
+                        "versionId": 2,
+                        "proofOfInclusionBTreeSerialized": "{\"t\":3,\"root\":{\"isLeaf\":true,\"keys\":[1,3],\"children\":[]}}"
+                    },
+                    "payload": {
+                        "lock": {
+                            "versionId": 0,
+                            "proofOfInclusionBTreeSerialized": "{\"t\":3,\"root\":{\"isLeaf\":true,\"keys\":[3],\"children\":[]}}"
+                        },
+                        "object": {
+                            "id": 3,
+                            "recordStatus": "ACTIVE",
+                            "platformAccountId": "abc123",
+                            "platformAPIKey": "xyz321"
+                        }
+                    }
+                }
+            })
+        });
+    });
+    it('handles RequestedAccountList 2x', async () => {
+        const { producer, eventProcessor } = getDependencies();
         await eventProcessor.processEvent({
             value: Buffer.from(
                 JSON.stringify({
@@ -98,7 +167,7 @@ describe('EventProcessor', () => {
                             lock: {
                                 versionId: 0,
                                 proofOfInclusionBTreeSerialized:
-                                    '{"t":3,"root":{"isLeaf":true,"keys":[2],"children":[]}}',
+                                    '{"t":3,"root":{"isLeaf":false,"keys":[4],"children":[{"isLeaf":true,"keys":[2,3],"children":[]},{"isLeaf":true,"keys":[5,6,7],"children":[]}]}}',
                             },
                             object: {
                                 id: 1,
@@ -111,7 +180,7 @@ describe('EventProcessor', () => {
                             lock: {
                                 versionId: 0,
                                 proofOfInclusionBTreeSerialized:
-                                    '{"t":3,"root":{"isLeaf":true,"keys":[2],"children":[]}}',
+                                    '{"t":3,"root":{"isLeaf":true,"keys":[4],"children":[]}}',
                             },
                             object: {
                                 id: 2,
